d85340954bb21d015ea37fe323dd5cb3fdd6056d
Elena39 <eelleennaa3939@gmail.com>
2026-03-24 10:18:12 +0800

    修改了版本号

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -99,7 +99,7 @@ import { Box, HomeFilled, List, WarningFilled, Setting, Document, Lock, Folder, 
 import { useWatcherStore } from './stores'
 
 // 直接定义版本号（确保每次构建都会更新）
-const appVersion = '1.6.4'
+const appVersion = '1.0.2'
 
 const watcherStore = useWatcherStore()
 const conflictCount = ref(0)
