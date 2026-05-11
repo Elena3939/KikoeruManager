4077b8407775395a7a9aea539c7800d6f93490db
Elena <1072345383@qq.com>
2026-05-11 21:59:57 +0800

    版本号升至 1.4.0

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -204,7 +204,7 @@ import SystemPromptHost from './components/system/SystemPromptHost.vue'
 import NotificationBell from './components/system/NotificationBell.vue'
 import router from './router'
 
-const appVersion = '1.3.0'
+const appVersion = '1.4.0'
 const route = useRoute()
 const watcherStore = useWatcherStore()
 const conflictCount = ref(0)
